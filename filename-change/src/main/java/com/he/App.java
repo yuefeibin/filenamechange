6b9bd2fa1551be66108526273d6e2edbc03fd2fa
yuefeibin <53465920+yuefeibin@users.noreply.github.com>
2020-08-28 17:23:16 +0800

    Add files via upload
    上传批量文件名修改

---
+++ filename-change/src/main/java/com/he/App.java
@@ -0,0 +1,34 @@
+package com.he;
+
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) {
+        String[] sourceName = {"RJ-001","XJ-001"};
+        String[] distName = {"RJ-002","XJ-002"};
+        File sourceDir = new File("E:\\中电科技\\distt");
+        File distDir = new File("E:\\dist");
+        x(sourceName, distName, sourceDir, distDir);
+    }
+    private static void x(String[] sourceName, String[] distName, File sourceDir, File distDir) {
+        if (sourceDir.isDirectory()) {
+            File[] files = sourceDir.listFiles();
+            for (File oldFile : files) {
+                String absolutePath = oldFile.getAbsolutePath();
+                String replace = absolutePath.replace(sourceDir.getAbsolutePath(), distDir.getAbsolutePath());
+                for (int i = 0; i < sourceName.length; i++) {
+                    replace=replace.replaceAll(sourceName[i],distName[i]);
+                }
+                File newFile = new File(replace);
+                if (oldFile.isDirectory()) {
+                    newFile.mkdirs();
+                    x(sourceName,distName,oldFile,newFile);
+                } else {
+                    FileUtil.copy(oldFile, newFile, true);
+                }
+            }
+        }
+    }
+}
